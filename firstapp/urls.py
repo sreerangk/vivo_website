3f5bbc9429172cb5f83ea39b51330f6450bef992
sreerangk <sreerangk77@gmail.com>
2022-06-30 11:58:48 +0530

    nav

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('base', views.base, name='base'),
+    path('GHJK', views.index, name='index'),
+    path('', views.base, name='base'),
 ]
